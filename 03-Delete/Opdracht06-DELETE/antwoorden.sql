0d1356674c9f8fc85f5b341a28d48fa93be053ac
Kugapalan <kugapalan.ganeshwaran@student.rocva.nl>
2022-01-25 19:56:29 +0100

    Opdracht afgerond

--- 03-Delete/Opdracht06-DELETE/antwoorden.sql
+++ 03-Delete/Opdracht06-DELETE/antwoorden.sql
@@ -1,11 +1,11 @@
 -- Opdracht 1 
-DELETE FROM players WHERE name = "david silva";
+DELETE FROM players WHERE name = "David Silva";
 
 -- Opdracht 2 
-DELETE FROM players WHERE club = "willem II"
+DELETE FROM players WHERE club = "Willem II"
 
 -- Opdracht 3
-DELETE FROM players WHERE nationality = "belgium" AND club = "fc barcelona"
+DELETE FROM players WHERE nationality = "Belgium" AND club = "fc barcelona"
 
 -- Opdracht 4
 DELETE FROM players WHERE age >= 38
