399fdbd35258a4f669c177dc80712b473dcb4335
Kugapalan <kugapalan.ganeshwaran@student.rocva.nl>
2022-01-26 14:57:03 +0100

    Opdracht afgerond

--- eindopdracht/antwoorden.sql
+++ eindopdracht/antwoorden.sql
@@ -120,40 +120,16 @@ Pokemon X/Pokemon Y 	3DS
 
 
 -- Opdracht 5
-127.0.0.1/mod-mysql-basic-opdracht/videogamesales/		http://localhost/phpmyadmin/index.php?route=/table/sql&db=mod-mysql-basic-opdracht&table=videogamesales
+127.0.0.1/mod-mysql-basic-opdracht/videogamesales/		http://localhost/phpmyadmin/index.php?route=/database/sql&db=mod-mysql-basic-opdracht
 
-   Weergave van records 0 - 24 (998 totaal, Query duurde 0,0008 seconden.)
-
-
-SELECT * FROM videogamesales WHERE Global_Sales>0;
+   Weergave van records 0 -  0 (1 totaal, Query duurde 0,0010 seconden.)
 
 
-id	name	platform	year	genre	publisher	NA_Sales	EU_Sales	JP_Sales	Other_Sales	Global_Sales	
-1	Wii Sports 	Wii 	2006	Sports 	Nintendo 	4149	2902	377	846	8274	
-2	Super Mario Bros. 	NES 	1985	Platform 	Nintendo 	2908	358	681	77	4024	
-3	Mario Kart Wii 	Wii 	2008	Racing 	Nintendo 	1585	1288	379	331	3582	
-4	Wii Sports Resort 	Wii 	2009	Sports 	Nintendo 	1575	1101	328	296	33	
-5	Pokemon Red/Pokemon Blue 	GB 	1996	Role-Playing 	Nintendo 	1127	889	1022	1	3137	
-6	Tetris 	GB 	1989	Puzzle 	Nintendo 	232	226	422	58	3026	
-7	New Super Mario Bros. 	DS 	2006	Platform 	Nintendo 	1138	923	65	29	3001	
-8	Wii Play 	Wii 	2006	Misc 	Nintendo 	1403	92	293	285	2902	
-9	New Super Mario Bros. Wii 	Wii 	2009	Platform 	Nintendo 	1459	706	47	226	2862	
-10	Duck Hunt 	NES 	1984	Shooter 	Nintendo 	2693	63	28	47	2831	
-11	Nintendogs 	DS 	2005	Simulation 	Nintendo 	907	11	193	275	2476	
-12	Mario Kart DS 	DS 	2005	Racing 	Nintendo 	981	757	413	192	2342	
-13	Pokemon Gold/Pokemon Silver 	GB 	1999	Role-Playing 	Nintendo 	9	618	72	71	231	
-14	Wii Fit 	Wii 	2007	Sports 	Nintendo 	894	803	36	215	2272	
-15	Wii Fit Plus 	Wii 	2009	Sports 	Nintendo 	909	859	253	179	22	
-16	Kinect Adventures! 	X360 	2010	Misc 	Microsoft Game Studios 	1497	494	24	167	2182	
-17	Grand Theft Auto V 	PS3 	2013	Action 	Take-Two Interactive 	701	927	97	414	214	
-18	Grand Theft Auto: San Andreas 	PS2 	2004	Action 	Take-Two Interactive 	943	4	41	1057	2081	
-19	Super Mario World 	SNES 	1990	Platform 	Nintendo 	1278	375	354	55	2061	
-20	Brain Age: Train Your Brain in Minutes a Day 	DS 	2005	Misc 	Nintendo 	475	926	416	205	2022	
-21	Pokemon Diamond/Pokemon Pearl 	DS 	2006	Role-Playing 	Nintendo 	642	452	604	137	1836	
-22	Super Mario Land 	GB 	1989	Platform 	Nintendo 	1083	271	418	42	1814	
-23	Super Mario Bros. 3 	NES 	1988	Platform 	Nintendo 	954	344	384	46	1728	
-24	Grand Theft Auto V 	X360 	2013	Action 	Take-Two Interactive 	963	531	6	138	1638	
-25	Grand Theft Auto: Vice City 	PS2 	2002	Action 	Take-Two Interactive 	841	549	47	178	1615	
+SELECT  name, MAX(Global_Sales) FROM videogamesales;
+
+
+
+Wii Sports 	8274	
 
 -- Opdracht 6 
 127.0.0.1/mod-mysql-basic-opdracht/videogamesales/		http://localhost/phpmyadmin/index.php?route=/table/sql&db=mod-mysql-basic-opdracht&table=videogamesales
@@ -190,55 +166,56 @@ SELECT COUNT(*) FROM videogamesales WHERE publisher = "nintendo";
 228	
 
 -- Opdracht 9
-127.0.0.1/mod-mysql-basic-opdracht/videogamesales/		http://localhost/phpmyadmin/index.php?route=/table/sql&db=mod-mysql-basic-opdracht&table=videogamesales
+127.0.0.1/mod-mysql-basic-opdracht/videogamesales/		http://localhost/phpmyadmin/index.php?route=/database/sql&db=mod-mysql-basic-opdracht
 
-   Weergave van records 0 - 24 (77 totaal, Query duurde 0,0008 seconden.)
+   Weergave van records 0 - 24 (74 totaal, Query duurde 0,0008 seconden.)
 
 
-SELECT name, year FROM videogamesales WHERE genre = "racing" OR publisher = "Nintendo" AND publisher = "Activision";
+SELECT name, year FROM videogamesales WHERE genre = "racing" AND publisher = "Nintendo" OR publisher = "Activision";
 
 
 name	year	
 Mario Kart Wii 	2008	
 Mario Kart DS 	2005	
-Gran Turismo 3: A-Spec 	2001	
+Call of Duty: Modern Warfare 3 	2011	
+Call of Duty: Black Ops 	2010	
+Call of Duty: Black Ops 3 	2015	
+Call of Duty: Modern Warfare 2 	2009	
+Call of Duty: Modern Warfare 3 	2011	
+Call of Duty: Black Ops 	2010	
 Mario Kart 7 	2011	
-Gran Turismo 4 	2004	
-Gran Turismo 	1997	
-Gran Turismo 5 	2010	
-Mario Kart 64 	1996	
-Gran Turismo 2 	1999	
-Super Mario Kart 	1992	
-Need for Speed Underground 	2003	
-Mario Kart 8 	2014	
-Mario Kart: Double Dash!! 	2003	
-Need for Speed Underground 2 	2004	
-Forza Motorsport 3 	2009	
-Mario Kart: Super Circuit 	2001	
-Diddy Kong Racing 	1997	
-Crash Team Racing 	1999	
-The Simpsons: Hit & Run 	2003	
-Forza Motorsport 4 	2011	
-Need for Speed: Most Wanted 	2005	
-Gran Turismo 5 Prologue 	2007	
-Excitebike 	1984	
-Forza Motorsport 2 	2007	
-MotorStorm 	2006	
+Call of Duty: Modern Warfare 2 	2009	
+Call of Duty: Ghosts 	2013	
+Call of Duty: Ghosts 	2013	
+Call of Duty 4: Modern Warfare 	2007	
+Call of Duty: Advanced Warfare 	2014	
+Call of Duty: World at War 	2008	
+Call of Duty: Black Ops 3 	2015	
+Call of Duty 4: Modern Warfare 	2007	
+World of Warcraft 	2004	
+Destiny 	2014	
+Call of Duty: World at War 	2008	
+Call of Duty: Advanced Warfare 	2014	
+Tony Hawk's Pro Skater 	1999	
+Guitar Hero III: Legends of Rock 	2007	
+StarCraft II: Wings of Liberty 	2010	
+Tony Hawk's Pro Skater 2 	2000	
 
 -- Opdracht 10
-127.0.0.1/mod-mysql-basic-opdracht/videogamesales/		http://localhost/phpmyadmin/index.php?route=/table/sql&db=mod-mysql-basic-opdracht&table=videogamesales
+127.0.0.1/mod-mysql-basic-opdracht/videogamesales/		http://localhost/phpmyadmin/index.php?route=/database/sql&db=mod-mysql-basic-opdracht
 
-   Weergave van records 0 -  0 (1 totaal, Query duurde 0,0027 seconden.)
-
-
-SELECT AVG(NA_Sales) AS Gemiddelde_aantal_verkoop_Noord_Amerika, AVG(EU_Sales) AS gemiddelde_aantal_verkoop_Europa, AVG(JP_Sales) AS Gemiddelde_aantal_verkoop_japan FROM videogamesales;
+   Weergave van records 0 -  0 (1 totaal, Query duurde 0,0011 seconden.)
 
 
+SELECT ROUND(AVG(NA_Sales)) AS Gemiddelde_aantal_verkoop_Noord_Amerika, ROUND(AVG(EU_Sales)) AS gemiddelde_aantal_verkoop_Europa, ROUND(AVG(JP_Sales)) AS Gemiddelde_aantal_verkoop_japan FROM videogamesales;
 
-191.1932	114.9890	49.6496	
+
+
+201	121	39	
+
 
 -- Opdracht 11
-DELETE FROM videogamesales WHERE name = "halo 2"
+DELETE FROM videogamesales WHERE name="Halo 2" AND platform="XB"
 
 -- Opdracht 12
 DELETE FROM videogamesales WHERE year = 2012 OR publisher = "ubisoft"
